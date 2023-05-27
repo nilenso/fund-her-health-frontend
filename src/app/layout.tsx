cc907e1f5753b20a5223615afd7a204e99d58c52
Alisha Mohanty <alishamohanty1416@gmail.com>
2023-05-27 19:06:09 +0530

    Navbar and Landing Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,7 @@
+'use client'
 import './globals.css'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Fund Her Health',
-  description: '',
-}
+import { ChakraProvider } from '@chakra-ui/react'
+import WithSubnavigation from '../components/navbar'
 
 export default function RootLayout({
   children,
@@ -16,12 +10,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-      <main className={styles.main}>
-      <div className={styles.description}>
-        {children}
-      </div>
-    </main></body>
+      <body>
+        <ChakraProvider>
+          <WithSubnavigation />
+          <main>
+            {children}
+          </main>
+        </ChakraProvider>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,54 @@
+'use client'
+import {
+  Box,
+  Heading,
+  Container,
+  Text,
+  Button,
+  Stack,
+} from '@chakra-ui/react';
+
 export default function Home() {
   return (
-    <div>Hello</div>
+    <>
+      <Container maxW={'3xl'}>
+        <Stack
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}>
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+            lineHeight={'110%'}>
+            <Text color={'green.500'}>Fund Her Health</Text>
+            <Text as={'span'}
+              fontSize={{ base: 'xl', sm: '2xl', md: '4xl' }} >
+              Empowering HPV Vaccination Initiatives
+            </Text>
+          </Heading>
+          <Text color={'gray.500'}>
+            Join us in driving widespread HPV vaccination through CSR funding
+          </Text>
+          <Stack
+            direction={'column'}
+            spacing={3}
+            align={'center'}
+            alignSelf={'center'}
+            position={'relative'}>
+            <Button
+              colorScheme={'green'}
+              bg={'green.400'}
+              rounded={'full'}
+              px={6}
+              _hover={{
+                bg: 'green.500',
+              }}>
+              Campaigns
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
+    </>
   )
 }
